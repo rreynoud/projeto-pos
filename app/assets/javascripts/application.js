d654e1c4fa56fc12412fbcba5e34f05ebae25364
Rafael <rreynoud@gmail.com>
2019-06-18 15:56:46 -0300

    Ajustes

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -52,36 +52,49 @@ function add_new_product_to_list_test()
     console.log("Preço do produto")
     console.log(price)
 
-    var rowCount = $("#sampleTable > tbody").children().length;
+    var row_count = $("#sampleTable > tbody").children().length;
     var product_id = $('.select.optional option:selected').val();
     var product_name = $('.select.optional option:selected').text();
     var quantity = $('.numeric').val();
 
-    var position = rowCount
+    var position = row_count
     --position
 
-    var nameQuantity = "list[items_attributes]["+ position +"][quantity]"
+    var name_quantity = "list[items_attributes]["+ position +"][quantity]"
+    var name_product_id = "list[items_attributes]["+ position +"][product_id]"
+    var name_current_price = "list[items_attributes]["+ price +"][current_price]"
 
-    var nameProductId = "list[items_attributes]["+ position +"][product_id]"
-
-    var innerProductId = "<input type=\"text\"   name=\"" +  nameProductId + "\" " +
+    var inner_product_id = "<input type=\"text\"   name=\"" +  name_product_id + "\" " +
         "id=\"" + "product_"+ product_id + "\" value=\"" + product_id  + "\">"
 
-    var quantityInput = "<input type=\"text\" name=\"" +  nameQuantity + "\" " +
+    var quantity_input = "<input type=\"text\" name=\"" +  name_quantity + "\" " +
         "id=\"" + "quantity_"+ product_id + "\" value=\"" + quantity  + "\">"
 
-    var x=document.getElementById('sampleTable').insertRow(rowCount);
-    var productId = x.insertCell(0);
-    var productCell = x.insertCell(1);
-    var quantityCell = x.insertCell(2);
-    var priceCell = x.insertCell(3);
-    var totalValueCell = x.insertCell(4);
 
-    productId.innerHTML=innerProductId;
-    productCell.innerHTML=product_name;
-    quantityCell.innerHTML= quantityInput ;
-    priceCell.innerHTML= price ;
-    totalValueCell.innerHTML = quantity * price  ;
+    // var currentPriceInput = "<input type=\"text\"   name=\"" +  name_current_price + "\" " +
+    //     "id=\"" + "product_"+ product_id + "\" value=\"" + product_id  + "\">"
+
+
+    var totalItem = quantity * price
+
+    var listTotal = parseFloat($('#list_total').val())
+
+    listTotal += totalItem
+
+    $('#list_total').attr('value', listTotal)
+
+    var x=document.getElementById('sampleTable').insertRow(row_count);
+    var product_id = x.insertCell(0);
+    var product_cell = x.insertCell(1);
+    var quantity_cell = x.insertCell(2);
+    var price_cell = x.insertCell(3);
+    var total_value_cell = x.insertCell(4);
+
+    product_id.innerHTML=inner_product_id;
+    product_cell.innerHTML=product_name;
+    quantity_cell.innerHTML= quantity_input ;
+    price_cell.innerHTML= price ;
+    total_value_cell.innerHTML = totalItem  ;
 
     $(".numeric").val("0");
 }
