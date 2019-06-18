41e70b58d059c63c71ca59b802205ba8a37f4603
Rafael <rreynoud@gmail.com>
2019-06-18 11:23:53 -0300

    Salvando os items com suas respectivas quantidades

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -41,19 +41,34 @@ function add_new_product_to_list_test()
     var product_id = $('.select.optional option:selected').val();
     var product_name = $('.select.optional option:selected').text();
     var quantity = $('.numeric').val();
+    console.log(quantity)
+
+    var position = rowCount
+    --position
+
+    var nameQuantity = "list[items_attributes]["+ position +"][quantity]"
+
+    var nameProductId = "list[items_attributes]["+ position +"][product_id]"
 
 
-    var innerProductId = "<input type=\"text\" id=\"" + "product_"+ product_id + "\" value=\"" + product_id  + "\">"
-    var quantityInput = "<input type=\"text\" id=\"" + "quantity_"+ product_id + "\" value=\"" + quantity  + "\">"
+    var innerProductId = "<input type=\"text\"   name=\"" +  nameProductId + "\" " +
+        "id=\"" + "product_"+ product_id + "\" value=\"" + product_id  + "\">"
+
+    var quantityInput = "<input type=\"text\" name=\"" +  nameQuantity + "\" " +
+        "id=\"" + "quantity_"+ product_id + "\" value=\"" + quantity  + "\">"
+
 
     var x=document.getElementById('sampleTable').insertRow(rowCount);
-    var productCell = x.insertCell(0);
-    var quantityId = x.insertCell(1);
-    var quantityCell = x.insertCell(2);
-    var totalValueCell = x.insertCell(3);
+    var productId = x.insertCell(0);
+    var productCell = x.insertCell(1);
+    var quantity = x.insertCell(2);
+    var quantityCell = x.insertCell(3);
+    var totalValueCell = x.insertCell(4);
+
+    productId.innerHTML=innerProductId;
     productCell.innerHTML=product_name;
-    quantityId.innerHTML= innerProductId ;
-    quantityCell.innerHTML= quantityInput ;
+    quantity.innerHTML= quantityInput ;
+    quantityCell.innerHTML= innerProductId ;
     totalValueCell.innerHTML= quantityInput ;
 
     $(".numeric").val("0");
